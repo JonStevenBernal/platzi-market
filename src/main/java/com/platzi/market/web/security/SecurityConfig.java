954be9bf29c150cd9c6c1f7d4dfe0e13340421b4
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2022-10-24 20:39:27 -0500

    Se actualiza la configuracion por razon deprecated

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.platzi.market.web.security;
 
 import com.platzi.market.domain.service.MarketUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @EnableWebSecurity // Class to charge the Security
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private MarketUserDetailsService marketUserDetailsService;
 
@@ -15,8 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.marketUserDetailsService = marketUserDetailsService;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(marketUserDetailsService);
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http)  throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(marketUserDetailsService)
+                .and()
+                .build();
     }
 }
